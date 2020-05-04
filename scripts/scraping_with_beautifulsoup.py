2f71ae8109314e5a0b8004d00003af57085434a3
William Lopes <willyamlopes@gmail.com>
2020-05-04 18:50:55 +0100

    Update the code and add description about the code

--- scripts/scraping_with_beautifulsoup.py
+++ scripts/scraping_with_beautifulsoup.py
@@ -1,17 +1,30 @@
+'''
+BeautifulSoup 
+Is a library for extracting data from HTML and XML files. It is particularly useful in that it does not require the source markup to be well-formed. For example, it deals with missing tags and incorrect nesting as smoothly as possible.
+
+Urllib 
+Is a library for open, reading, and parsing URLs
+'''
+
+'''
+This program opens an url read it with urllib and then parse it using BeautifulSoup. After that, it will find all of the span tags, grap their value and then sum them
+'''
+
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup as beautify
 import re
 
-url = 'http://py4e-data.dr-chuck.net/comments_483694.html' # input('Enter -> ')
+# url = input("Enter url: ")
+url = 'http://py4e-data.dr-chuck.net/comments_483694.html'
 html = urllib.request.urlopen(url).read()
 soup = beautify(html, 'html.parser')
 
 lst = list()
 count = 0
-tags = soup.find_all('span')
+tags = soup.find_all("span")
 lst = re.findall('>([0-9]+?)<', str(tags))
 
-for i in range(len(lst)):
-    count = count + int(lst[i])
+for i in lst:
+    count = count + int(i)
 
 print(count)
