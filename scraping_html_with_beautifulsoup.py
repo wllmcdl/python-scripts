c07a96b7fd365db02038a41349419a5125837840
William Lopes <willyamlopes@gmail.com>
2020-05-04 14:13:11 +0100

    Add import alias

--- scraping_html_with_beautifulsoup.py
+++ scraping_html_with_beautifulsoup.py
@@ -1,10 +1,10 @@
 import urllib.request, urllib.parse, urllib.error
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as beautify
 import re
 
 url = 'http://py4e-data.dr-chuck.net/comments_483694.html' # input('Enter -> ')
 html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
+soup = beautify(html, 'html.parser')
 
 lst = list()
 count = 0
